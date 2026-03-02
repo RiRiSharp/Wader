fc45716d5ba503ba7d6e192fbf7d2b91597b2e1e
RiRiSharp <github@ririsharp.nl>
2026-03-02 22:35:44 +0100

    Add Match parameter to NavLink

--- src/Wader.Bootstrap/Components/NavBar/BsNavLink.razor.cs
+++ src/Wader.Bootstrap/Components/NavBar/BsNavLink.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 using Wader.Bootstrap.BaseComponents;
 
 namespace Wader.Bootstrap.Components.NavBar;
@@ -9,6 +10,9 @@ public partial class BsNavLink : BsChildContentComponent
 
     [CascadingParameter]
     private BsDropdownOptions? DropdownOptions { get; set; }
+
+    [Parameter]
+    public NavLinkMatch Match { get; set; }
 
     private string DropdownOptionsClass => DropdownOptions?.ToNavLinkBootstrapClass() ?? "";
 }
