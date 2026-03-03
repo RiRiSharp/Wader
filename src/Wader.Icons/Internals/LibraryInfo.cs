9ea8c8700695aca744253d66bc135964ba0a86a7
RiRiSharp <github@ririsharp.nl>
2026-03-03 20:14:10 +0100

    Fix double slash issue

--- src/Wader.Icons/Internals/LibraryInfo.cs
+++ src/Wader.Icons/Internals/LibraryInfo.cs
@@ -4,5 +4,6 @@ internal static class LibraryInfo
 {
     internal static readonly string Name =
         typeof(LibraryInfo).Assembly.GetName().Name ?? throw new ArgumentNullException();
-    internal static readonly string RootPath = $"./_content/{Name}/";
+
+    internal static readonly string RootPath = $"./_content/{Name}";
 }
