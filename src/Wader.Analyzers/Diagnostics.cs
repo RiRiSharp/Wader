d7b77c951746b7f389518600d598f1ab1a63aad2
RiRiSharp <github@ririsharp.nl>
2026-04-18 10:46:29 +0200

    Set up wishlist for analyzers

---
+++ src/Wader.Analyzers/Diagnostics.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.Language;
+
+namespace Wader.Analyzers;
+
+internal static class Diagnostics
+{
+    public const string IdRequireParenthesesRazorExpression = "WADER001";
+    public static readonly RazorDiagnosticDescriptor RequireParathesesRazorExpression = new(
+        IdRequireParenthesesRazorExpression,
+        () => "Razor expressions must be parenthesized",
+        RazorDiagnosticSeverity.Warning
+    );
+
+    public const string IdRequireNameCascadingParameter = "WADER002";
+    public static readonly RazorDiagnosticDescriptor RequireNameCascadingParameter = new(
+        IdRequireNameCascadingParameter,
+        () => "Razor expressions must be parenthesized",
+        RazorDiagnosticSeverity.Warning
+    );
+}
