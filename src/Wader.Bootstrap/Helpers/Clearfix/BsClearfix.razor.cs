e6fd8a1cdea5135d93dfc7dffc3585a2d62f9f87
RiRiSharp <github@ririsharp.nl>
2026-07-28 21:31:09 +0200

    Add clearfix

---
+++ src/Wader.Bootstrap/Helpers/Clearfix/BsClearfix.razor.cs
@@ -0,0 +1,8 @@
+﻿using Wader.Bootstrap.Internal.BaseComponents;
+
+namespace Wader.Bootstrap.Helpers.Clearfix;
+
+public partial class BsClearfix : BsChildContentComponent
+{
+    protected override string BsComponentClasses => "clearfix";
+}
---
+++ tests/Wader.Bootstrap.Tests/Helpers/BsClearfixTests.cs
@@ -0,0 +1,6 @@
+using Wader.Bootstrap.Helpers.Clearfix;
+using Wader.Bootstrap.Tests.TestUtilities;
+
+namespace Wader.Bootstrap.Tests.Helpers;
+
+public class BsClearfixTests() : BsComponentTests<BsClearfix>("""<div class="clearfix {0}" {1}></div>""");
